06e1d23bbe6740ad7b39d254662124b752d41c33
REDXICAN <andres.xbgo@outlook.com>
2025-08-05 17:45:23 -0600

    Changes made:
    Removed apt-get - Vercel doesn't support it
    Removed build_web.sh reference - using direct Flutter command instead
    Added environment variables directly to build command

--- build_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Location: Project root (same as pubspec.yaml)
-# Build script for Vercel deployment
-
-# Build Flutter web with environment variables from Vercel
-flutter build web --release \
-  --dart-define=SUPABASE_URL=$SUPABASE_URL \
-  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY \
-  --web-renderer html
